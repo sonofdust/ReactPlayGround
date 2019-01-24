b67cf9247f78046d4e1fef31fe9368e333b2fff0
Nicholas D. Roman <NicholasDRoman@hotmail.com>
2019-01-24 07:16:02 -0700

    Adding npm validator module

--- src/app.js
+++ src/app.js
@@ -1,11 +1,3 @@
-import subtract, { square, add } from "./utils.js";
-import isSenior, { isAdult, canDrink } from "./person.js";
+import validator from "validator";
 
-console.log("app.js is running!!!");
-console.log(square(5));
-console.log(add(12, 98));
-console.log(subtract(98, 18));
-
-// console.log(isAdult(5));
-// console.log(canDrink(98));
-console.log(isSenior(76) ? "I  am old!!" : "I am younr!!");
+console.log(validator.isEmail('Nicholas@hotmail.con'));
